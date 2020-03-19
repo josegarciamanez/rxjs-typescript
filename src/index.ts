814c38eb888e11088bafbd25b4cba5d92abcd455
Jose García Máñez <josegarciamanez@gmail.com>
2020-03-19 19:33:29 +0100

    create fork

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,29 @@ import { Observable, Observer } from 'rxjs';
 
 // eslint-disable-next-line
 const observer: Observer<any> = {
-  next: value => console.log('next:', value),
-  error: error => console.warn('error:', error),
-  complete: () => console.info('completado')
+    next: value => console.log('next:', value),
+    error: error => console.warn('error:', error),
+    complete: () => console.info('completado')
 };
 
 const intervalo$ = new Observable<number>(subscriber => {
-  // Crear un contador 1, 2, 3, 5...
-  let count = 0;
-  const interval = setInterval(() => {
-    count++;
-    subscriber.next(count);
-  }, 1000);
-  return () => {
-    clearInterval(interval);
-    console.log('Intervalo Destruido');
-  };
+    // Crear un contador 1, 2, 3, 5...
+    let count = 0;
+    const interval = setInterval(() => {
+        count++;
+        subscriber.next(count);
+    }, 1000);
+    return () => {
+        clearInterval(interval);
+        console.log('Intervalo Destruido');
+    };
 });
 
 const subs = intervalo$.subscribe(num => console.log('Num:', num));
 
 setTimeout(() => {
-  subs.unsubscribe();
+    subs.unsubscribe();
 }, 3000);
-
 
 // eslint-disable-next-line
 document.getElementById('app').innerHTML = `
