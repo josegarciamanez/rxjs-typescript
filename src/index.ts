1532b7e9787a8c2367612df9df3a1972f50e4094
Jose García Máñez <josegarciamanez@gmail.com>
2020-03-20 09:41:34 +0100

    changes in index.html

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-// CSS IMPORTS
-import './main.css';
-
-// Rxjs Imports
 import {} from 'rxjs';
-
-// HTML
-document.getElementById('app').innerHTML = `
-<div class="container">
-<h1>RXJS Typescript</h1>
-</div>
-`;
